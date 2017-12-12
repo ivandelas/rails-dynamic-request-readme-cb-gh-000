5c8cce681de6e3db4c683d92dda3898fad58e559
ivandelas <ivandelas@gmail.com>
2017-12-12 20:07:26 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
+  #get 'post/show'
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :post, only: :show
   # Example resource route with options:
   #   resources :products do
   #     member do
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,17 +6,17 @@ describe 'navigate' do
   end
 
   it 'loads the show page' do
-    visit "/posts/#{@post.id}"
+    visit "/post/#{@post.id}"
     expect(page.status_code).to eq(200)
   end
 
   it 'shows the title on the show page in an h1 tag' do
-    visit "/posts/#{@post.id}"
+    visit "/post/#{@post.id}"
     expect(page).to have_css("h1", text: "My Post")
   end
 
   it 'shows the description on the show page in a p tag' do
-    visit "/posts/#{@post.id}"
+    visit "/post/#{@post.id}"
     expect(page).to have_css("p", text: "My post desc")
   end
 end
